12cde9994d7d9c9a2cb13cbb74e3dc80543b6827
ahead123 <arthurallenhead@yahoo.com>
2019-05-07 15:18:17 -0700

    finished user selection option cookie save

--- src/actions/selectionsActions/index.js
+++ src/actions/selectionsActions/index.js
@@ -1,5 +1,14 @@
 export const logChoices = (company_ids) => dispatch => {
-  console.log('companyId',company_ids);  
+  console.log('companyId',company_ids); 
+  if(company_ids.length >= 1){
+    let choices = company_ids.join(",");
+    localStorage.setItem('sh_gdpr_cookie', choices);
+  };  
   dispatch({ type: 'LOG_CHOICES_START'});
-  dispatch({ type: 'LOG_CHOICES_SUCCESS', payload: company_ids })
+  dispatch(logChociesSuccess({ company_ids, 'hide_modal': true }))
+  //dispatch({ type: 'LOG_CHOICES_SUCCESS', payload: company_ids, 'hide_modal': true })
+};
+
+const logChociesSuccess = (payload) => {
+  return { type: 'LOG_CHOICES_SUCCESS', payload }
 };
--- src/reducers/selectionsReducer/index.js
+++ src/reducers/selectionsReducer/index.js
@@ -2,7 +2,7 @@ export default (state={}, action) => {
   const { type, payload } = action;
   switch (type) {
     case 'LOG_CHOICES_SUCCESS':
-      return {...state, company_ids: payload}
+      return {...state, company_ids: payload.company_ids, 'hide_modal': payload.hide_modal }
     default:
       return state;
   }
